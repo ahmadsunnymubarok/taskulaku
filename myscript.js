6cef84e7affacf39d8831f7b42daf0efaf4d3138
ahmadsunnymubarok <husnimubarokslur@gmail.com>
2024-04-26 17:19:01 +0700

    menambhkan fitur keranjang dan modal deatil box

--- myscript.js
+++ myscript.js
@@ -14,12 +14,20 @@ document.querySelector("#search-button").onclick = (e) => {
   searchBox.focus();
   e.preventDefault();
 };
+// toggle clas  active untuk shopping-cart
+
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-btn").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
 
 //  klik diluar sidebar untuk mengehilangkan nav
 // klik di luar menu
 
 const hamMenu = document.querySelector("#menu");
 const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-btn");
 
 document.addEventListener("click", function (e) {
   if (!hamMenu.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -29,4 +37,33 @@ document.addEventListener("click", function (e) {
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
   }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// modal box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klil close  modal icon
+
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+//  close modal sembarang tempat
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
